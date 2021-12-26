dd2c83b78c71e91a9540fd63ad924f253314edde
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2021-12-26 21:55:17 +0530

    Created home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,6 +6,54 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/background.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height*0.15,
+                  width: MediaQuery.of(context).size.width*0.5,
+                  child: Container(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.07,
+                width: MediaQuery.of(context).size.width*0.8,
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.08),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(onPressed: (){}, child: Text("Sign Up")),
+                  ElevatedButton(onPressed: (){}, child: Text("Sign In"))
+                ],
+              ),)
+
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-
-class signUpWidget extends StatelessWidget {
+class SignUpWidget extends StatelessWidget {
+  const SignUpWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/background.png"),
+                    fit: BoxFit.cover)),
+          )
+        ],
+      ),
+    );
   }
 }
