5887065744b23ab6eb61da0ce83b07ada3ff498a
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2021-12-26 13:26:11 +0530

    Added colors,themes, dependencies

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+var darkScaffoldColor = const Color(0xff282D36);
+var darkAccentColor = const Color(0xffFFC388);
+var darkPrimaryTextColor = Colors.white;
+var darkSecondaryTextColor = const Color(0xffFFC388);
+var darkTertiaryTextColor = const Color(0xff282D36);
+var darkSecondAccentColor = const Color(0xffFFD390);
+var successColor = const Color(0xffB2E081);
+var errorColor = const Color.fromARGB(255, 255, 167, 167);
+
+//var darkHintColor = Colors.black87;
--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -1,1 +1,37 @@
+import 'package:events_app/utils/colors.dart' as colors;
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class darkTheme {
+  static Future<ThemeData> get DarkTheme async => ThemeData(
+        fontFamily: 'Poppins',
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: colors.darkScaffoldColor,
+        textTheme: TextTheme(
+          headline1: GoogleFonts.poppins(
+              color: Colors.white, fontSize: 40, fontWeight: FontWeight.w200),
+          bodyText1: GoogleFonts.poppins(
+              color: Colors.white, fontSize: 15, fontWeight: FontWeight.w100),
+          bodyText2: GoogleFonts.poppins(
+              color: Colors.white, fontSize: 15, fontWeight: FontWeight.w300),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          enabledBorder: UnderlineInputBorder(
+              borderSide:
+                  BorderSide(color: colors.darkAccentColor, width: 1.0)),
+          focusedBorder: UnderlineInputBorder(
+              borderSide:
+                  BorderSide(color: colors.darkAccentColor, width: 1.0)),
+          border: UnderlineInputBorder(
+            borderSide: BorderSide(color: colors.darkAccentColor, width: 1.0),
+          ),
+          contentPadding: const EdgeInsets.all(7.0),
+          fillColor: colors.darkScaffoldColor,
+          labelStyle: GoogleFonts.poppins(color: colors.darkPrimaryTextColor),
+          floatingLabelStyle:
+              GoogleFonts.poppins(color: colors.darkPrimaryTextColor),
+          hintStyle: GoogleFonts.poppins(color: colors.darkPrimaryTextColor),
+        ),
+      );
+}
