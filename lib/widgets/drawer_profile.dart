7a3a9d82e50c1050f5af8021bf1ea9e73a2552c4
sooryasrajan <soorya.s27@gmail.com>
2022-01-24 20:11:43 +0530

    Changed drawer_profile.dart to use ListTile for more responsive page

--- lib/widgets/drawer_profile.dart
+++ lib/widgets/drawer_profile.dart
@@ -1,4 +1,3 @@
-
 import 'package:events_app/screens/profile_page.dart';
 import 'package:events_app/screens/sign_in.dart';
 import 'package:events_app/screens/starred_events.dart';
@@ -15,154 +14,111 @@ class NavigationDrawer extends StatelessWidget {
     return Drawer(
       child: Material(
         color: colors.scaffoldColor,
-        child: ListView(
-          padding: EdgeInsets.zero,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-             Container(
-               decoration: BoxDecoration(
-                   gradient: LinearGradient(
-                     begin: Alignment.topRight,
-                     end: Alignment.bottomLeft,
-                     colors: [
-                       colors.accentColor,
-                       colors.secondAccentColor
-                     ],
-                   )
-               ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: CircleAvatar(
-                    radius: 70,
-                    child: ClipOval(
-                      child:  SvgPicture.asset('assets/svg/female.svg'),
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [colors.accentColor, colors.secondAccentColor],
+              )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: CircleAvatar(
+                        radius: 70,
+                        child: ClipOval(
+                          child: SvgPicture.asset('assets/svg/female.svg'),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                'Sumithra Sudhakar',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                    fontSize: 20,
-                    color: colors.primaryTextColor,
-                    fontWeight: FontWeight.w600),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child:  Directionality(
-                textDirection: TextDirection.rtl,
-
-                child: ElevatedButton.icon(
-                  icon: const Icon(
-                    Icons.person_rounded,
-                    size: 13,
-                  ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const ProfilePage()));
-                  },
-                  label: Padding(
-                    padding:  const EdgeInsets.fromLTRB( 8, 8, 100, 8),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
                     child: Text(
-    'PROFILE',
-    style: GoogleFonts.poppins(
-    fontSize: 15,
-    color: colors.secondaryTextColor,
-    fontWeight: FontWeight.w600),
-    ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(80.0)),
-                    padding: const EdgeInsets.all(0.0),
-                    primary: colors.accentColor
-                  ),
-                ),
-              ),
-            ),
-
-            //second button
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child:  Directionality(
-                textDirection: TextDirection.rtl,
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const StarredWidget()));
-                  },
-                  icon: const Icon(
-                    Icons.star,
-                    size: 13,
-                  ),
-                  label: Padding(
-                    padding:   const EdgeInsets.fromLTRB( 8, 8, 30, 8),
-                    child: Text(
-                      'STARRED EVENTS',
+                      'Sumithra Sudhakar',
                       style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          color: colors.secondaryTextColor,
+                          fontSize: 20,
+                          color: colors.primaryTextColor,
                           fontWeight: FontWeight.w600),
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(80.0)),
-                    padding: const EdgeInsets.all(0.0),
-                    primary: colors.accentColor
-
-                  ),
-                ),
+                ],
               ),
             ),
-
-//third button
+            buildDrawerItem(
+                icon: Icons.person_rounded,
+                text: "Profile",
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ProfilePage()));
+                }),
+            buildDrawerItem(
+                icon: Icons.star,
+                text: "Starred Events",
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const StarredWidget()));
+                }),
+            Expanded(child: Container()),
             Padding(
-              padding: const EdgeInsets.fromLTRB( 8, 200, 8, 8),
-              child:  Directionality(
-                textDirection: TextDirection.rtl,
-                child: ElevatedButton.icon(
-                  icon:  const Icon(
-                    Icons.logout,
-                    size: 13,
-
-                  ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const SignInWidget()));
-                  },
-
-                  label:  Padding(
-                    padding: const EdgeInsets.fromLTRB( 8, 8, 80, 8),
-                    child: Text(
-                      'LOG OUT',
-                      style: GoogleFonts.poppins(
-
-                          fontSize: 15,
-                          color: colors.secondaryTextColor,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(80.0)),
-                    padding: const EdgeInsets.all(0.0),
-                    primary: colors.accentColor
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Container(
+                height: 1,
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: colors.accentColor),
                   ),
                 ),
               ),
             ),
-
+            buildDrawerItem(
+                icon: Icons.logout,
+                text: "Log Out",
+                onTap: () {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (context) => const SignInWidget()),
+                      (Route<dynamic> route) => false);
+                }),
           ],
         ),
       ),
     );
   }
+}
+
+Widget buildDrawerItem({
+  required String text,
+  required IconData icon,
+  VoidCallback? onTap,
+}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 5),
+    child: ListTile(
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Icon(
+          icon,
+          color: colors.accentColor,
+        ),
+      ),
+      title: Text(
+        text,
+        style: GoogleFonts.poppins(color: colors.primaryTextColor),
+      ),
+      onTap: onTap ?? () {},
+    ),
+  );
 }
