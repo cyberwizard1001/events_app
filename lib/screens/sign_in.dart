f620b88700b642450deda2aa63701104db57bfde
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-01-26 01:01:09 +0530

    Fixed sign in

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -22,81 +22,80 @@ class SignInWidget extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SingleChildScrollView(
-                  physics: const ScrollPhysics(),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.32,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            'Sign in',
-                            style: GoogleFonts.poppins(
-                                color: colors.primaryTextColor,
-                                fontSize: 35,
-                                fontWeight: FontWeight.w500),
-                          ),
+                      Expanded(child: Container(),flex: 2,),
+                      Expanded(
+                        flex: 3,
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 20),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  'Sign in',
+                                  style: GoogleFonts.poppins(
+                                      color: colors.primaryTextColor,
+                                      fontSize: 35,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                              const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                              child: TextFormField(
+                                  decoration: InputDecoration(
+                                    label: const Text('Username'),
+                                    filled: true,
+                                    hintText: 'Please enter your username',
+                                    hintStyle: GoogleFonts.poppins(
+                                        color: colors.primaryTextColor.withOpacity(0.7)),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide.none,
+                                        borderRadius: BorderRadius.circular(5)),
+                                    fillColor: colors.textBoxFill,
+                                    focusColor: colors.textBoxFill,
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide.none,
+                                        borderRadius: BorderRadius.circular(5)),
+                                  )),
+                            ),
+                            Padding(
+                              padding:
+                              const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  label: const Text('Password'),
+                                  filled: true,
+                                  hintText: 'Please enter your password',
+                                  hintStyle: GoogleFonts.poppins(
+                                      color:
+                                      colors.primaryTextColor.withOpacity(0.7)),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide.none,
+                                      borderRadius: BorderRadius.circular(5)),
+                                  fillColor: colors.textBoxFill,
+                                  focusColor: colors.textBoxFill,
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide.none,
+                                      borderRadius: BorderRadius.circular(5)),
+                                ),
+                              ),
+                            ),
+                            GradientButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => const Home()));
+                              },
+                              title: 'SIGN IN',
+                            ),
+                          ],
                         ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                        child: TextFormField(
-                            decoration: InputDecoration(
-                          label: const Text('Username'),
-                          filled: true,
-                          hintText: 'Please enter your username',
-                          hintStyle: GoogleFonts.poppins(
-                              color: colors.primaryTextColor.withOpacity(0.7)),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(5)),
-                          fillColor: colors.textBoxFill,
-                          focusColor: colors.textBoxFill,
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(5)),
-                        )),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            label: const Text('Password'),
-                            filled: true,
-                            hintText: 'Please enter your password',
-                            hintStyle: GoogleFonts.poppins(
-                                color:
-                                    colors.primaryTextColor.withOpacity(0.7)),
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.circular(5)),
-                            fillColor: colors.textBoxFill,
-                            focusColor: colors.textBoxFill,
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-                        ),
-                      ),
-                      GradientButton(
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => const Home()));
-                        },
-                        title: 'SIGN IN',
-                      ),
+                      )
                     ],
-                  ))
+                  )
             ],
           )
-        ],
-      ),
     );
   }
 }
